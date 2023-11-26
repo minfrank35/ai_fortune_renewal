ccbd83f6ddc37439df1e121063b6eb11c8ca17dd
Jominsu <8wkdltkd@gmail.com>
2023-11-26 18:17:24 +0900

    jdbc 수정

--- src/main/java/com/example/azure/springazuredemo/controller/Member.java
+++ src/main/java/com/example/azure/springazuredemo/controller/Member.java
@@ -1,23 +1,19 @@
 package com.example.azure.springazuredemo.controller;
 
 
-import jakarta.persistence.*;
+
 import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
 @Getter
 @Setter
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tmp_seq")
-    @SequenceGenerator(name = "tmp_seq", sequenceName = "tmp_seq", allocationSize = 1)
+
     private Long id;
 
-    @Column(unique = true)
     private String email;
 
     private String pwd;
--- src/main/java/com/example/azure/springazuredemo/controller/MemberRepositoryImpl.java
+++ src/main/java/com/example/azure/springazuredemo/controller/MemberRepositoryImpl.java
@@ -1,9 +1,39 @@
 package com.example.azure.springazuredemo.controller;
 
+import java.sql.*;
+
 public class MemberRepositoryImpl implements MemberRepository {
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String USER = "sys as sysdba";
+    private static final String PASSWORD = "1234";
+
+
     @Override
     public boolean saveMember(Member member) {
+        try {
+            // 오라클 데이터베이스 연결
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
 
-        return false;
+            // 쿼리 수행
+            String query = "INSERT INFO member(id, email, pwd, name, gender, birthday,phone) VALUES (?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement insertStatement = connection.prepareStatement(query)) {
+
+                insertStatement.setLong(1,member.getId());
+                insertStatement.setString(2, member.getEmail());
+                insertStatement.setString(3,member.getPwd());
+                insertStatement.setString(4, member.getName());
+                insertStatement.setString(5, member.getGender());
+                insertStatement.setString(6, member.getBirthday());
+                insertStatement.setString(7, member.getPhone());
+            }
+
+            // 연결 종료
+            connection.close();
+        } catch (
+                SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+       return true;
     }
 }
